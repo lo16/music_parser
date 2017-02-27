899b36d99cd304c27cc5a4043f90eccd11e517f4
lo16 <cheeyanglo@gmail.com>
2017-02-27 21:17:51 +0800

    add starter code to calculate distance matrix

---
+++ dist_matrix.py
@@ -0,0 +1,25 @@
+from pyAudioAnalysis import audioBasicIO
+from pyAudioAnalysis import audioFeatureExtraction
+import matplotlib.pyplot as plt
+import numpy as np 
+
+#here I use a single channel conversion of Koi
+#to use a stereo copy simply average the two channels (for now)
+[Fs, x] = audioBasicIO.readAudioFile("Koi_single_channel.wav");
+
+#TODO: tweak sample length
+F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs);
+
+#retrieve MFCCs from F and take the transpose for convenience
+MFCC = np.array(F[8:20]).T
+
+#calculate distance matrix (VERY EXPENSIVE)
+dist_matrix = [[np.linalg.norm(i - j) for j in xrange(len(MFCC))] for i in xrange(len(MFCC))]
+#dist_matrix = np.zeros((len(MFCC), len(MFCC)))
+
+
+fig, ax = plt.subplots(figsize=(200,200))
+cax = ax.matshow(dist_matrix, interpolation='nearest')
+ax.grid(True)
+plt.title('MFCC similarity matrix')
+plt.show()
